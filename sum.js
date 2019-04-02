e81c8f055da596d760c78979fbc123d22fee2303
Nima Boscarino <nima.boscarino@gmail.com>
2019-04-02 17:35:50 +0000

    sum up whole numbers, whether positive or negative

--- sum.js
+++ sum.js
@@ -3,15 +3,30 @@ var justTheNumbers = allArguments.slice(2)
 
 // console log all of the arguments!
 
-console.log('PROCESS ARGV:', allArguments)
+// console.log('PROCESS ARGV:', allArguments)
 
 // only want everything but the first two. How do I do this?
 
-console.log('SLICED!', justTheNumbers)
+// console.log('SLICED!', justTheNumbers)
+
+var sum = 0
 
 // some kind of for loop to go through the numbers
 
 for (var i = 0; i < justTheNumbers.length; i++) {
-	var number = justTheNumbers[i]
-	console.log(number)
+	var numberString = justTheNumbers[i]
+	// turn that number which is a string into an actual number
+
+	var number = Number(numberString)
+
+	sum = sum + number
+
+	// console.log('type of number', typeof number)
+
+	// console.log(number)
 }
+
+console.log(sum)
+
+
+
