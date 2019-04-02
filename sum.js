f4e2cd4d46a1af918ca12f9ba54e375ea66278f7
Nima Boscarino <nima.boscarino@gmail.com>
2019-04-02 17:20:57 +0000

    do a loop over just the numbers

--- sum.js
+++ sum.js
@@ -1,3 +1,17 @@
+var allArguments = process.argv
+var justTheNumbers = allArguments.slice(2)
+
 // console log all of the arguments!
 
-console.log('PROCESS ARGV:', process.argv)
+console.log('PROCESS ARGV:', allArguments)
+
+// only want everything but the first two. How do I do this?
+
+console.log('SLICED!', justTheNumbers)
+
+// some kind of for loop to go through the numbers
+
+for (var i = 0; i < justTheNumbers.length; i++) {
+	var number = justTheNumbers[i]
+	console.log(number)
+}
